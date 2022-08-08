cb55b28d41ab2243c368db77522ad50412a55850
Simon B <simon.birkholz@uni-ulm.de>
2022-08-08 09:50:40 +0200

    added debugging script

--- check_candidate_pool.py
+++ check_candidate_pool.py
@@ -1,7 +1,46 @@
 import joblib
 import glob
+import json
 
 DATA_DIR = 'C:/Users/Simon/Desktop/prefetcher_outputs'
+
+RERANKER_FILE = 'C:/Users/Simon/Desktop/reranker_test_citing_ids.joblib'
+reranker_citing_ids = joblib.load(RERANKER_FILE)
+
+def load_papers_info(file):
+    papers_info = dict()
+
+    with open(file) as f:
+        for line in f:
+            entry = json.loads(line.strip())
+
+            keep_keys = ['paper_title', 'paper_abstract', 'paper_year']
+
+            papers_info[entry['paper_id']] = dict([(k, v) for k, v in entry.items() if k in keep_keys])
+
+    return papers_info
+
+paper_info = load_papers_info('papers.jsonl')
+
+
+def do_joblib_check(data: dict):
+    citing_papers = set()
+
+    for k in data.keys():
+        citing_papers.add(k)
+
+    too_much_ids = citing_papers - reranker_citing_ids
+
+    missing_ids = reranker_citing_ids - citing_papers
+
+
+    print(f"Citing papers {len(citing_papers)}")
+    print(f"Missing papers {len(missing_ids)}")
+    #for id in missing_ids:
+    #    print(paper_info[id])
+    print(f"To Much papers {len(too_much_ids)}")
+    #for id in too_much_ids:
+    #    print(paper_info[id])
 
 
 if __name__ == '__main__':
@@ -10,4 +49,4 @@ if __name__ == '__main__':
 
     for file in job_files:
         data = joblib.load(file)
-        print(data)
+        do_joblib_check(data)
